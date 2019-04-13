b0de557f49753179ad2a400431b97966def48440
Koichi Taura <taura.koichi@gmail.com>
2019-04-13 22:53:04 +0900

    modify path

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import firebase from "firebase";
-
-config = {
-    apiKey: "AIzaSyBxYRO0esh6j8jp2PFAvOYHnzauBqbXebg",
-    authDomain: "taurin190com-1baea.firebaseapp.com",
-    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-    storageBucket: "<BUCKET>.appspot.com",
-};
-firebase.initializeApp(config);
 
 Vue.config.productionTip = false
 
