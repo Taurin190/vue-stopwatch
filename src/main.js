b7819b54b7fc02d87b91e54c414412daabbb3c1e
Koichi Taura <taura.koichi@gmail.com>
2019-04-13 22:26:45 +0900

    add docs directory for github pages

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from "firebase";
+
+config = {
+    apiKey: "AIzaSyBxYRO0esh6j8jp2PFAvOYHnzauBqbXebg",
+    authDomain: "taurin190com-1baea.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+};
+firebase.initializeApp(config);
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
